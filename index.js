69254b63f96a26a428f2c5ba13dd7b55a0afaa5f
smith <andras@sexybuddha.hu>
2018-08-29 10:11:41 +0200

    🔅 turn off light on exit

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ process.env.NODE_ENV = 'production';
 
 const noble = require('noble-uwp');
 const getColor = require('screen-avg-color');
+const nodeCleanup = require('node-cleanup');
 
-const peripheralIdOrAddress = process.argv[2].toLowerCase();
 const SERVICE_UUID = 'ff06';
 const CHARACTERISTIC_UUID = 'fffc';
 const SATURATION = 0;
+const peripheralIdOrAddress = process.argv.length >= 3 ? process.argv[2].toLowerCase() : null;
+
+let colorHandler = null;
+let exiting = false;
+
+if (!peripheralIdOrAddress) {
+    console.error('Peripheral ID missing');
+    process.exit(9);
+}
 
 noble.on('stateChange', state => {
     if (state === 'poweredOn') {
@@ -37,38 +46,41 @@ noble.on('discover', peripheral => {
         }
 
         if (txPowerLevel) {
-            console.log('  TX Power Level    = ' + txPowerLevel);
+            console.debug('  TX Power Level    = ' + txPowerLevel);
         }
 
         if (manufacturerData) {
-            console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
+            console.debug('  Manufacturer Data = ' + manufacturerData.toString('hex'));
         }
 
         if (serviceData) {
-            console.log('  Service Data      = ' + JSON.stringify(serviceData, null, 2));
+            console.debug('  Service Data      = ' + JSON.stringify(serviceData, null, 2));
         }
 
         if (serviceUuids) {
-            console.log('  Service UUIDs     = ' + serviceUuids);
+            console.debug('  Service UUIDs     = ' + serviceUuids);
         }
 
         init(peripheral);
     }
 });
 
-let colorHandler = null;
+
 function init(peripheral) {
     peripheral.once('disconnect', () => {
-        console.warn('Peripheral disconnected');
+        console.warn('Disconnected');
         colorHandler = null;
-        noble.startScanning();
+
+        if (!exiting) noble.startScanning();
     });
 
     peripheral.connect(err => {
         if (err) console.error(err);
+        else console.log('Connected');
 
         peripheral.discoverSomeServicesAndCharacteristics([SERVICE_UUID], [CHARACTERISTIC_UUID], (err, services, characteristics) => {
             if (err) console.error(err);
+            else console.debug('Services discovered: ' + characteristics.length);
 
             colorHandler = characteristics.find(c => c.uuid === CHARACTERISTIC_UUID);
 
@@ -93,3 +105,34 @@ function matchCurrentScreenColor() {
         });
     });
 }
+
+function setWhiteLevel(value = 0, cb = null) {
+    if (!colorHandler) {
+        if (cb) cb();
+        return;
+    }
+
+    const colorData = Buffer.from([value, 0, 0, 0]);
+
+    colorHandler.write(colorData, true, err => {
+        if (err) console.error(err);
+        if (cb) cb();
+    });
+}
+
+nodeCleanup((exitCode, signal) => {
+    exiting = true;
+
+    setWhiteLevel(0, () => {
+        if (colorHandler) {
+            noble.disconnect(colorHandler._peripheralId);
+            colorHandler = null;
+        }
+
+        process.kill(process.pid, signal);
+    });
+
+    nodeCleanup.uninstall();
+
+    return false;
+});
